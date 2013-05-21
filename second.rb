7f30a15926cedbc63ce93f424f86371bea250cf0
barunthapa <barunthapa.bvdt@gmail.com>
2013-05-21 09:46:20 +0545

    Third Commit

--- second.rb
+++ second.rb
@@ -1,1 +1,3 @@
-this is the second file to commit.  
+e the switch context button in the upper left corner of this screen to switch between your personal context (anaida07) and any organizations you are a member of.
+  
+  After you switch contexts you’ll see an organization-focused dashboard that lists out organization repositories an.  
