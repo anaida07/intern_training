488f78e8a2d2ddd90c9c90238e86e74bdf66be69
barunthapa <barunthapa.bvdt@gmail.com>
2013-05-21 09:51:57 +0545

    Third Commit

--- second.rb
+++ second.rb
@@ -1,3 +1,3 @@
-e the switch context button in the upper left corner of this screen to switch between your personal context (anaida07) and any organizations you are a member of.
-  
+anizations you are a member of.
+dkfjghdkfjghkdfjghdkfjghkdfghdkfjgkdjghkdjghkdjhg 
   After you switch contexts you’ll see an organization-focused dashboard that lists out organization repositories an.  
